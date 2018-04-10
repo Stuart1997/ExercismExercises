57cf81de6c413058b493968b1bee7713a434d890
Stuart Purcell <stuart.purcell@digital.hmrc.gov.uk>
2018-04-10 13:42:09 +0100

    WordCount - Implemented a validated word counting method using regex, tested multiple scenarios

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -1,5 +1,19 @@
 package wordcount
 
-object WordCount extends App {
+import scala.collection.immutable.ListMap
 
+object WordCount extends App {
+  def CountWords(sentence: String): Option[Map[String, Int]] = {
+    val lowerCaseSentence = sentence.toLowerCase()                              //Removes case issues (e.g. Hello and hello will no longer be seen as different words)
+    val noMultiSpaces = lowerCaseSentence.replaceAll("""([ ]{2,})""", " ")      //Removes any instances of 2 or more spaces and makes them a single space
+    val noSpecialCharacters = noMultiSpaces.replaceAll("""([^a-z\s])""", "")    //Removes everything that isn't the letters a-z or whitespace
+
+    if (noSpecialCharacters != "" && noSpecialCharacters != " ") {
+      val sentenceAsList = noSpecialCharacters.split(" ").toList                //Adds each individual word to a list
+      val mappedWords = sentenceAsList.groupBy(identity).mapValues(_.size)      //Maps each word with how many times it is found in the list
+      Some(ListMap(mappedWords.toSeq.sortWith(_._2 > _._2):_*))                 //Orders the words by how many times they appear (highest to lowest)
+    } else {
+      None
+    }
+  }
 }
--- src/test/scala/WordCountSpecs.scala
+++ src/test/scala/WordCountSpecs.scala
@@ -2,5 +2,59 @@ import org.scalatest.{Matchers, WordSpec}
 import wordcount._
 
 class WordCountSpecs extends WordSpec with Matchers {
-  
+  "A string containing only unique words" should {
+    "return each word mapped with 1" in {
+      val string = "My name is Stuart"
+      val expectedResult = Some(Map("my" -> 1, "name" -> 1, "is" -> 1, "stuart" -> 1))
+      WordCount.CountWords(string) shouldBe expectedResult
+    }
+  }
+
+  "A string containing duplicate words (only appearing twice)" should {
+    "return each word that is duplicated mapped with 2, and the others with 1" in {
+      val string = "My name is Stuart, what is your name?"
+      val expectedResult = Some(Map("name" -> 2, "is" -> 2, "my" -> 1, "stuart" -> 1, "what" -> 1, "your" -> 1))
+      WordCount.CountWords(string) shouldBe expectedResult
+    }
+  }
+
+  "A string containing only one word 10 times" should {
+    "return only that word mapped with 10" in {
+      val string = "Mine mine mine mine mine mine mine mine mine mine"
+      val expectedResult = Some(Map("mine" -> 10))
+      WordCount.CountWords(string) shouldBe expectedResult
+    }
+  }
+
+  "A string in which there is more than a single space" should {
+    "reduce that whitespace to a single space" in {
+      val string = "My  name   is    Stuart"
+      val expectedResult = Some(Map("my" -> 1, "name" -> 1, "is" -> 1, "stuart" -> 1))
+      WordCount.CountWords(string) shouldBe expectedResult
+    }
+  }
+
+  "An empty string" should {
+    "return none" in {
+      val string = ""
+      val expectedResult = None
+      WordCount.CountWords(string) shouldBe expectedResult
+    }
+  }
+
+  "A string containing only whitespace" should {
+    "return none" in {
+      val string = "    "
+      val expectedResult = None
+      WordCount.CountWords(string) shouldBe expectedResult
+    }
+  }
+
+  "A string containing only non-a-z characters" should {
+    "return none" in {
+      val string = "£123,456.78!?"
+      val expectedResult = None
+      WordCount.CountWords(string) shouldBe expectedResult
+    }
+  }
 }
