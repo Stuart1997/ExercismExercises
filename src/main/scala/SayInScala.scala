bc82f37c58fe3f73d5b367918a8db600c87083f8
Stuart Purcell <stuart.purcell@digital.hmrc.gov.uk>
2018-05-03 08:47:27 +0100

    SayInScala - Totally refactored the readNumber method and modified it to not include the word 'and' anywhere for now

--- src/main/scala/SayInScala.scala
+++ src/main/scala/SayInScala.scala
@@ -1,39 +1,69 @@
+package sayinscala
 
 object SayInScala extends App {
 
   def readNumber(number: Int): String = {
-    val oneToNine = List("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-    val tenToNineteen = List("ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen")
-    val multiplesOf10 = List("twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety")
-    val higherThan100 = List("hundred", "thousand")
+    val oneToNine = Map(
+      0 -> "",
+      1 -> "one", 2 -> "two", 3 -> "three",
+      4 -> "four", 5 -> "five", 6 -> "six",
+      7 -> "seven", 8 -> "eight", 9 -> "nine")
 
-    val digits = number.toString.length
-    val numberAsString = number.toString
+    val tenToNineteen = Map(
+      10 -> "ten", 11 -> "eleven", 12 -> "twelve", 13 -> "thirteen", 14 -> "fourteen",
+      15 -> "fifteen", 16 -> "sixteen", 17 -> "seventeen", 18 -> "eighteen", 19 -> "nineteen")
 
-    val result = digits match {
-      case oneDigit if oneDigit == 1 => {
-        if (number == 0) "zero"
-        else oneToNine(number - 1)
-      }
+    val multiplesOfTen = Map(
+      20 -> "twenty", 30 -> "thirty", 40 -> "forty", 50 -> "fifty",
+      60 -> "sixty", 70 -> "seventy", 80 -> "eighty", 90 -> "ninety")
 
-      case twoDigits if twoDigits == 2 => {
-        if (number >= 10 && number < 20) tenToNineteen(numberAsString.tail.toInt)
-        else {
-          if (numberAsString.endsWith("0")) multiplesOf10(numberAsString.head.toInt - 1)
-          else multiplesOf10(numberAsString.head.toString.toInt - 2) + " " + oneToNine(numberAsString.tail.toString.toInt - 1)
+    val zeroes = StringBuilder.newBuilder                                 //Makes a mutable list
+    val intAsStringList = number.toString.toList                          //Puts the number passed in into a list of strings (chars)
+    val toNine = 9 - intAsStringList.size                                 //Works out how many 0s need to be put in front of the number
+    for (x <- 1 to toNine) zeroes.append("0")                             //Add that many 0s to a list
+    val nineDigits = zeroes.toList ::: intAsStringList                    //Combine those lists so that it becomes e.g. 0000xxxxx
+    val firstNonZeroIndex = nineDigits.indexWhere(index => index != '0')  //Finds the first position in which the number actually begins
+    val digitGroup = nineDigits.grouped(3).toList                         //Splits the number into 3 groups of 3 digits
+
+    def block3(group: Int, amount: String): String = {
+      val ones = digitGroup(group)(2).toString.toInt
+      val tens =
+        if (digitGroup(group)(1).toString == "1") {
+          digitGroup(group)(1).toString + digitGroup(group)(2).toString
+        } else if (digitGroup(group)(1).toString == "0") {
+          "0"
+        } else {
+          digitGroup(group)(1).toString + "0"
         }
-      }
-
-      case threeDigits if threeDigits == 3 => {
-        if (numberAsString.endsWith("00")) oneToNine(numberAsString.head.toString.toInt - 1) + " " + higherThan100.head
-        else
-          oneToNine(numberAsString.head.toString.toInt - 1) + " " + higherThan100.head + " and " +
-          multiplesOf10(numberAsString.charAt(1).toString.toInt - 2) + " " + oneToNine(numberAsString.last.toString.toInt - 1)
-      }
-
-      case _ => "Invalid number"
+      val hundred = digitGroup(group).head.toString.toInt
+      val hundredWord = if (hundred > 0) oneToNine(hundred) + s" $amount " else ""
+      val tensWord =
+        if (tens.head == '0') {
+          ""
+        }
+        else if (tens.head == '1') {
+          tenToNineteen(tens.toInt)
+        }
+        else {
+          multiplesOfTen(tens.toInt)
+        }
+      val onesWord = if (tensWord.contains("ten") || tensWord.contains("eleven") || tensWord.contains("twelve") || tensWord.contains("teen")) "" else oneToNine(ones)
+      val sentence = s"$hundredWord $tensWord $onesWord"
+      sentence
     }
 
-    result
+    val result = firstNonZeroIndex match {
+      case zero if number == 0 => "zero"
+      case millions if firstNonZeroIndex < 3 && firstNonZeroIndex >= 0 => "million" //block1(0, "million")
+      case thousands if firstNonZeroIndex < 6 && firstNonZeroIndex >= 3 => "thousand" //block2(1, "thousand")
+      case hundreds if firstNonZeroIndex < 9 && firstNonZeroIndex >= 6 => block3(2, "hundred")
+    }
+
+    val noMultiSpaces = result.replaceAll("""([ ]{2,})""", " ")      //Removes any instances of 2 or more spaces and makes them a single space
+    val noLeadingSpace = noMultiSpaces.replaceAll("""\A\s+""", "")   //Removes any leading whitespace at the beginning of the string
+    val noTrailingSpace = noLeadingSpace.replaceAll("""\s+\Z""", "")   //Removes any leading whitespace at the beginning of the string
+    noTrailingSpace
   }
+
+  println(readNumber(50))
 }
--- src/test/scala/SayInScalaSpecs.scala
+++ src/test/scala/SayInScalaSpecs.scala
@@ -1,4 +1,5 @@
 import org.scalatest.{Matchers, WordSpec}
+import sayinscala.SayInScala
 
 class SayInScalaSpecs extends WordSpec with Matchers {
   "0" should {
@@ -37,9 +38,93 @@ class SayInScalaSpecs extends WordSpec with Matchers {
   }
 
   "859" should {
-    "return 'eight hundred and fifty nine'" in {
+    "return 'eight hundred fifty nine'" in {
       val number = 859
-      SayInScala.readNumber(number) shouldBe "eight hundred and fifty nine"
+      SayInScala.readNumber(number) shouldBe "eight hundred fifty nine"
+    }
+  }
+
+  "1000" should {
+    "return 'one thousand'" in {
+      val number = 1000
+      SayInScala.readNumber(number) shouldBe "one thousand"
+    }
+  }
+
+  "1100" should {
+    "return 'one thousand one hundred'" in {
+      val number = 1100
+      SayInScala.readNumber(number) shouldBe "one thousand one hundred"
+    }
+  }
+
+  "1110" should {
+    "return 'one thousand one hundred ten'" in {
+      val number = 1110
+      SayInScala.readNumber(number) shouldBe "one thousand one hundred ten"
+    }
+  }
+
+  "1111" should {
+    "return 'one thousand one hundred eleven'" in {
+      val number = 1111
+      SayInScala.readNumber(number) shouldBe "one thousand one hundred eleven"
+    }
+  }
+
+  "1001" should {
+    "return 'one thousand one'" in {
+      val number = 1001
+      SayInScala.readNumber(number) shouldBe "one thousand one"
+    }
+  }
+
+  "1010" should {
+    "return 'one thousand ten'" in {
+      val number = 1010
+      SayInScala.readNumber(number) shouldBe "one thousand ten"
+    }
+  }
+
+  "1011" should {
+    "return 'one thousand eleven'" in {
+      val number = 1011
+      SayInScala.readNumber(number) shouldBe "one thousand eleven"
+    }
+  }
+
+  "10000" should {
+    "return 'ten thousand'" in {
+      val number = 10000
+      SayInScala.readNumber(number) shouldBe "ten thousand"
+    }
+  }
+
+  "11000" should {
+    "return 'eleven thousand'" in {
+      val number = 11000
+      SayInScala.readNumber(number) shouldBe "eleven thousand"
+    }
+  }
+
+  "12300" should {
+    "return 'twelve thousand three hundred'" in {
+      val number = 12300
+      SayInScala.readNumber(number) shouldBe "twelve thousand three hundred"
+    }
+  }
+
+  "12340" should {
+    "return 'twelve thousand three hundred forty'" in {
+      val number = 12340
+      SayInScala.readNumber(number) shouldBe "twelve thousand three hundred forty"
+    }
+  }
+
+  "12345" should {
+    "return 'twelve thousand three hundred forty five'" in {
+      val number = 12345
+      SayInScala.readNumber(number) shouldBe "twelve thousand three hundred forty five"
     }
   }
 }
